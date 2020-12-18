2733eab07cc7a5168b48dac1157a282aad0b241d
Kevin Killman <kevinkillman@gmail.com>
2020-12-18 17:22:49 -0500

    Interval Dropdown works. chart builds correctly from it

--- static/js/test.js
+++ static/js/test.js
@@ -11,15 +11,7 @@ d3.csv(`./ticker=${ticker}`, result => {
     chart = init_chart(ticker, result, 'Open', ctx);
 });
 
-function getData(ticker){
-    var data;
-    d3.csv(`./ticker=${ticker}`, result => {
-        cleanData(result)
-        data = result;
-        return data
-    });
-    return data;
-}
+
 
 buildPeriodDropdown().on("change", function(){
     buildIntervalDropdown(this.value)
@@ -40,18 +32,27 @@ function buildPeriodDropdown(){
 
 function buildIntervalDropdown(chosenPeriod) {
     var validIntervals;
-    if (chosenPeriod === "1d" | chosenPeriod === "5d" | chosenPeriod==="1mo"){
+    var intervalValues;
+    var selectArea = d3.select(".interval");
+    var intervalLabel = d3.select(".intervalLabel")
+    if (!(selectArea.empty())){
+        selectArea.remove()
+        intervalLabel.remove()
+    }
+    if (chosenPeriod === "1d" | chosenPeriod === "5d"){
         validIntervals = ['1 min','2 min','5 min','15 min','30 min','60 min','90 min','1 hour','1 day'];
+        intervalValues = ['1m','2m','5m','15m','30m','60m','90m','1h','1d']
     } else {
         validIntervals=['1d','5d','1wk','1mo','3mo']
+        intervalValues=['1d','5d','1wk','1mo','3mo']
     }
     let form = d3.select("#stockInputForm")
-    form.append('label').attr("for", "interval").text("Interval: ").classed("period", true)
-    let select = form.append('select').classed("period", true).attr("name", "interval")
+    form.append('label').attr("for", "interval").text("Interval: ").classed("intervalLabel", true)
+    let select = form.append('select').classed("interval", true).attr("name", "interval")
     
-    validIntervals.forEach(interval =>{
+    validIntervals.forEach((interval, index) =>{
         select.append('option')
-            .attr('value', `${interval}`)
+            .attr('value', `${intervalValues[index]}`)
             .classed("drop-text", true)
             .text(`${interval}`)
     })
@@ -62,9 +63,18 @@ function buildIntervalDropdown(chosenPeriod) {
 function stockButtonOn(){
     var chosenTicker = (this.stockInputForm.ticker.value.toUpperCase())
     var period = (this.stockInputForm.period.value)
-    d3.csv(`./ticker=${chosenTicker}/period=${period}`, result => {
+    var interval = (this.stockInputForm.interval.value)
+    d3.csv(`./ticker=${chosenTicker}/period=${period}/interval=${interval}`, result => {
         cleanData(result)
-        let labels = result.map(element => element.Date)
+        console.log(result)
+        let labels;
+        if (result[0].Date){
+            labels = result.map(element => element.Date)
+        }
+        if (result[0].Datetime){
+            labels = result.map(element => element.Datetime)
+        }
+
         let data = result.map(element=> element.Open)
         updateChart(chart, labels, data, chosenTicker)
     })
@@ -84,21 +94,27 @@ function init_chart(ticker, dataset, yVariable, ctx) {
                 data: yAxisData,
                 color: 'rgb(0, 200,0)',
                 fill: false,
-                pointRadius: 2,
+                pointRadius: .5,
                 pointHoverRadius: 3,
                 borderColor: 'rgb(50, 200,50)',
+                borderWidth: 2,
                 lineTension:0
 
             }]
         },
         options: {
-            
             scales:{
                 xAxes:[{
                     type: 'time',
-                    
+                    distribution: 'series',
+                    time: {
+                        displayFormats:{
+                            millisecond: 'h:mm a'
+                        }
+                    }
                 }]
             },
+            
             tooltips: {
                 mode: 'nearest'
             }
@@ -115,8 +131,14 @@ function cleanData(result) {
         element.Close = +element.Close ;
         element.High = +element.High ;
         element.Low = +element.Low ;
-        element.Date = (parseTime(element.Date));
+        if(element.Date){
+            element.Date = moment(element.Date);
+        }
+
         element.Volume = +element.Volume;
+        if (element.Datetime){
+            element.Datetime = moment(element.Datetime)
+        }
     });
 };
 
@@ -141,3 +163,14 @@ function updateChart(chart, newLabel, newData, ticker) {
 
     return chart;
 };
+
+
+function getData(ticker){
+    var data;
+    d3.csv(`./ticker=${ticker}`, result => {
+        cleanData(result)
+        data = result;
+        return data
+    });
+    return data;
+}
