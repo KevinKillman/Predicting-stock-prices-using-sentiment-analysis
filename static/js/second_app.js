90617f88889936b0382e56c4b5f8bb22e28b5a42
Kevin Killman <kevinkillman@gmail.com>
2020-12-22 10:34:09 -0500

    Late Nite Update

--- static/js/second_app.js
+++ static/js/second_app.js
@@ -2,30 +2,42 @@ function buildMostActive(){
     var container = d3.select(".mostActive")
     d3.json('./buildActive', function(response){
         response.forEach(answer => {
-            var buttonDiv = container.append('div').classed('containedButtonKevin', true)
-            let button = buttonDiv.append('button')
+            // var buttonDiv = container.append('div').classed('containedButtonKevin', true)
+            let button = container.append('button')
                     .attr('type', 'button')
-                    .classed('btn btn-primary btn-sm addDataClass', true)
+                    .classed('btn btn-primary btn-sm containedButtonKevin', true)
                     .attr('value', answer.Symbol)
                     .text('Add to Chart')
-            buttonDiv.append('h7').text(answer.Symbol).style('display', 'inline-block')
+            container.append('h7').text(answer.Symbol).style('display', 'inline-block')
+            container.append('br')
+            container.append('br')
+            
 
 
             button.on('click', function addChartData(){
                 let ticker = this.value;
                 d3.json(`./ticker=${ticker}/period=${globalPeriod}/interval=${globalInterval}`, function(response) {
                     cleanData(response)
-                    globalChart.data.datasets.push({
-                        label: `${ticker} Open Price`,
-                        data: response.map(el=>el.Open),
-                        fill: false,
-                        pointRadius: .9,
-                        pointHoverRadius: 3,
-                        borderColor: 'green',
-                        borderWidth: 1,
-                        lineTension:0
+                    let exists = false;
+                    globalChart.data.datasets.forEach((dataset) =>{
+                        if (dataset.label === `${ticker} Open Price`) {
+                            exists = true;
+                        }
                     })
+                    if (!exists){
+                        globalChart.data.datasets.push({
+                            label: `${ticker} Open Price`,
+                            data: response.map(el=>el.Open),
+                            fill: false,
+                            pointRadius: .9,
+                            pointHoverRadius: 3,
+                            borderColor: randomHsl(),
+                            borderWidth: 1,
+                            lineTension:0
+                        })
+                    }
                     globalChart.update()
+                    console.log(randomHsl())
                 })
             })
         })
@@ -33,3 +45,6 @@ function buildMostActive(){
 }
 
 
+function randomHsl() {
+    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+}
